515fa6d6c3415222d64c4c6e6249e14e9f86b999
gabriel.rabelo <gabriel.rabelo@edu.pucrs.br>
2022-04-01 23:14:06 -0300

    edita import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into usuario(id_usuario, ativo, email, nome, perfil, senha) values (1, true, 'lar_esperanca@email.com', 'Lar Esperanca', 'CASA', '$2a$10$4PGCcRpxerHdrt1ENiqwu.necdBFsbBtLSEe.ZtEsr0nhEg.gIyp2');
+insert into usuario(id, ativo, email, nome, perfil, senha) values (1, true, 'lar_esperanca@email.com', 'Lar Esperanca', 'CASA', '$2a$10$4PGCcRpxerHdrt1ENiqwu.necdBFsbBtLSEe.ZtEsr0nhEg.gIyp2');
 
-insert into usuario(id_usuario, ativo, email, nome, perfil, senha) values (2, true, 'admin@gmail.com', 'administrador', 'ADMIN', '$2a$10$KDZdDpsS30B0YID4riOWoeUwfvRB5lXRB8WOoNFiO/nidARaU4haC');
+insert into usuario(id, ativo, email, nome, perfil, senha) values (2, true, 'admin@gmail.com', 'administrador', 'ADMIN', '$2a$10$KDZdDpsS30B0YID4riOWoeUwfvRB5lXRB8WOoNFiO/nidARaU4haC');
