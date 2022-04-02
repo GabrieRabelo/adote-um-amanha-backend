f52e8de560996c3ca8e4e45eee2376bf7fc156f0
gabriel.rabelo <gabriel.rabelo@edu.pucrs.br>
2022-04-01 23:04:14 -0300

    adiciona testes de repositório.

--- src/main/java/br/com/ages/adoteumamanha/domain/entity/Endereco.java
+++ src/main/java/br/com/ages/adoteumamanha/domain/entity/Endereco.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(setterPrefix = "with")
-public class Endereco {
+public class Endereco implements Serializable {
 
     private String rua;
 
