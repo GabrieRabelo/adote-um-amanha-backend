6d35822026536fca5d621c03db6437f473e6f931
Marcelo Henrique De Souza <marcelo.souza.004@acad.pucrs.br>
2022-04-10 02:10:05 +0000

    Feature gerenciamento deletar / update necessidade

--- src/main/java/br/com/ages/adoteumamanha/exception/Mensagem.java
+++ src/main/java/br/com/ages/adoteumamanha/exception/Mensagem.java
@@ -11,6 +11,8 @@ public enum Mensagem {
     ACESSO_NAO_PERMITIDO("Credenciais de acesso inválida."),
     REQUEST_INVALIDO("Requisição inválida."),
     NECESSIDADE_NAO_ENCONTRADA("Necessidade não encontrada"),
+    PEDIDO_NAO_PODE_SER_DELETADO("Apenas o usuário criador do pedido poderá deleta-lo."),
+    STATUS_NAO_PENDENTE("É possivel alterar pedidos que estejam como status como pendente ainda."),
     SENHA_INVALIDA("Senha inválida."),
     ASSUNTO_INVALIDO("Assunto inválido."),
     DESCRICAO_INVALIDA("Descricão inválida."),
